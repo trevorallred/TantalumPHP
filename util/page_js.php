f41c592046c54c8d0e50e47a69cbfdc55ea41f06
Trevor Allred <trevorallred@gmail.com>
2011-01-25 11:21:36 -0800

    significant changes to the way the page is generated

--- util/page_js.php
+++ util/page_js.php
@@ -1,35 +1,60 @@
 <?php
+require_once("util/js_converter.php");
+
 /**
  * @param Model $model
- * @param boolean $root
  */
-function printStore($model, $root) {
+function createStore($model) {
+	$config = new JavaScriptObject();
+	if ($model->parent == NULL) {
+		$config->add("url", "data.php?id=" . $model->getId());
+	} else {
+		$config->addRaw("parentStore", $model->parent->getName() . "Store");
+	}
+	$config->addRaw("views", "{}");
+	$config->add("autoDestroy", true);
+	$config->add("batch", true);
+	$config->add("autoSave", false);
+	$config->add("pruneModifiedRecords", true);
+	$config->add("writer", "writer", JavaScriptValue::RAW);
+	$config->add("root", $model->getName());
+	$config->add("idProperty", $model->getPrimaryKey()->getName());
+	
+	$sortField = null;
+	$lowestOrder = 0;
+	foreach ($model->fields as $field) {
+		$sortOrder = $field->data["sortOrder"];
+		if ($sortOrder > 0 && ($lowestOrder == 0 || $sortOrder < $lowestOrder)) {
+			$sortField = $field;
+			$lowestOrder = $sortOrder;
+		}
+	}
+	if ($sortField != null) {
+		$sort = new JavaScriptObject();
+		$sort->add("field", $sortField->data["name"]);
+		$sort->add("direction", "ASC");
+		$config->add("sortInfo", $sort);
+	}
+	$config->add("currentRow", NULL);
+	
+	$fields = new JavaScriptArray();
+	foreach ($model->fields as $field) {
+		$fieldJS = new JavaScriptObject();
+		$fields->add($fieldJS);
+		$fieldJS->add("name", $field->getName());
+		switch ($field->data["basisColumnType"]) {
+			case "Integer":
+				$fieldJS->add("type", "integer");
+				break;
+		}
+	}
+	$config->add("fields", $fields);
 	?>
 	Tantalum.<?php echo $model->getName() ?>Store = Ext.extend(Ext.data.JsonStore, {
 		constructor : function(cfg) {
 			cfg = cfg || {};
-			Tantalum.<?php echo $model->getName() ?>Store.superclass.constructor.call(this, Ext.apply( {
-				<?php
-				if ($root) {
-					echo "url : 'data.php?id=" . $model->getId() ."', ";
-				}
-				?>
-				autoDestroy : true,
-				batch : true,
-				autoSave : false,
-				pruneModifiedRecords : true,
-				writer : writer,
-				root : '<?php echo $model->getName() ?>',
-				idProperty : '<?php echo $model->getPrimaryKey()->getName() ?>',
-				fields : [ <?php
-					$started = false;
-					foreach ($model->fields as $field) {
-						echo $started ? ", " : "";
-						echo "{ name: '" . $field->getName() ."'}";
-						$started = true;
-					}
-				?> ]
-			}, cfg));
+			Tantalum.<?php echo $model->getName() ?>Store.superclass.constructor.call(this, 
+				Ext.apply(<?php echo $config->printOut() ?>, cfg));
 		}
 	});
 	var <?php echo $model->getName() ?>Store = new Tantalum.<?php echo $model->getName() ?>Store();
@@ -60,98 +85,228 @@ function printStore($model, $root) {
 		
 		<?php
 		foreach ($model->childModels as $childModel) {
-			printStore($childModel, false);
+			createStore($childModel);
 		}
+	}
+	?>
+	function save<?php echo $model->getName() ?>Store() {
+		var savejson = new Object();
+		var savejsonModel = new Object();
+		saveQueue = new Object();
+		<?php echo $model->getName() ?>Store.save();
+		
+		for ( var action in saveQueue) {
+			if (Ext.isDefined(action)) {
+				savejsonModel[action] = [];
+				for ( var i = 0; i < saveQueue[action].length; i++) {
+					savejsonModel[action].push(saveQueue[action][i].data);
+				}
+			}
+		}
+		savejson['<?php echo $model->getName() ?>'] = savejsonModel;
+		
+		Ext.Ajax.request( {
+			url : 'data.php',
+			method: 'POST',
+			success : function(response, opts) {
+				var obj = Ext.decode(response.responseText);
+
+				if (obj.success === false) {
+					alert(obj.error_message);
+					return;
+				}
+				for ( var action in saveQueue) {
+					if (Ext.isDefined(action)) {
+						this['on' + Ext.util.Format.capitalize(action) + 'Records'](true, saveQueue[action],
+								obj.<?php echo $model->getName() ?>[action]);
+					}
+				}
+				<?php
+				foreach ($model->childModels as $childModel) {
+					echo "save".$childModel->getName()."Store(); ";
+				}
+				?>
+			},
+			failure : function(response, opts) {
+				alert("Failed to save data with status code " + response.status);
+			},
+			params : {
+				action : 'save',
+				id : '<?php echo $model->getId() ?>'
+			},
+			scope : <?php echo $model->getName() ?>Store,
+			jsonData : Ext.util.JSON.encode(savejson)
+		});
+	}
+	
+	function delete<?php echo $model->getName() ?>Store() {
+		var rows = item.grid.selModel.getSelections();
+		<?php echo $model->getName() ?>Store.remove(rows);
+	}
+
+	function insert<?php echo $model->getName() ?>Store() {
+		var store = <?php echo $model->getName() ?>Store;
+		var obj = store.recordType;
+		var o = new obj({ });
+		// o.data.DefineTableColumnDisplayOrder = 1;
+		// o.data.DefineTableColumnTableID = store.parentStore.data[store.parentStore.currentRow].DefineTableTableID;
+		// if(item.grid != null) {
+		//	item.grid.stopEditing();
+		// }
+		var size = store.data.length;
+		store.insert(size, o);
+		// if(item.grid != null) {
+		// 	item.grid.startEditing(size, 0);
+		// }
+	}
+	
+	<?php
+	
+	if ($model->parent == NULL) {
+		echo $model->getName() . "Store.load();\n";
+	}
+}
+
+function createView($view) {
+	if ($view->data["viewType"] == "Grid") {
+		createGrid($view);
+	}
+	if ($view->data["viewType"] == "Form") {
+		createForm($view);
+	}
+	foreach ($view->childViews as $childView) {
+		createView($childView);
 	}
 }
 
 /**
  * @param View $view
  */
+function createGrid($view) {
+	$config = new JavaScriptObject();
+	$config->add("flex", 1);
+	$config->add("stripeRows", TRUE);
+	$columnDefaults = new JavaScriptObject();
+	$columnDefaults->add("xtype", "gridcolumn");
+	$columnDefaults->add("width", 120);
+	$config->add("defaults", $columnDefaults);
+	$config->addRaw("store", $view->getModel()->getName() . "Store");
+	$config->addRaw("refresh", "function() {this.store.reload();}");
+	
+	$smFunction = "page.currentStore = ".$view->getModel()->getName()."Store;
+		page.currentSelectionModel = sm; ";
+	foreach ($view->getModel()->childModels as $childModel) {
+		$smFunction .= $childModel->getName()."Store.filter('DefineTableColumnTableID', record.data.DefineTableTableID); ";
+	};
+	$config->addRaw("sm", "new Ext.grid.RowSelectionModel( {
+		listeners : {
+			rowselect : {
+				fn : function(sm, index, record) {
+					$smFunction
+				}
+			}
+		}
+	})");
+	
+	$columns = new JavaScriptArray();
+	$config->add("columns", $columns);
+	foreach ($view->getFields() as $field) {
+		
+		$fieldJS = new JavaScriptObject();
+		$columns->add($fieldJS);
+		if ($field->data["visible"] == 0)  {
+			$fieldJS->add("hidden", TRUE);
+		}
+		$fieldJS->add("header", $field->data["label"]);
+		$fieldJS->add("sortable", TRUE);
+		
+		$width = 0;
+		if ($field->data["size"] > 0) {
+			$width = (int)$field->data["size"];
+		} else {
+			if ($field->data["displayType"] == "Checkbox") {
+				$width = 30;
+			}
+		}
+		$minWidth = 20 + (strlen($field->data["label"]) * 5);
+		if ($width > 0 && $width < $minWidth) {
+			$width = $minWidth;
+		}
+		if ($width > 0) {
+			$fieldJS->add("width", $width);
+		}
+		
+		$fieldJS->add("dataIndex", $field->data["name"]);
+		
+		if ($field->data["basisColumnType"] == "Integer") {
+			$fieldJS->add("xtype", "numbercolumn");
+			$fieldJS->add("format", "0");
+		}
+		
+		if ($field->data["editable"] == 1) {
+			if ($field->data["editor"]) {
+				$fieldJS->addRaw("editor", $field->data["editor"]);
+			} else {
+				$editor = new JavaScriptObject();
+				switch ($field->data["displayType"]) {
+					case "Checkbox":
+						$fieldJS->add("align", "center");
+						break;
+					case "Combo":
+						break;
+					default:
+						$editor->add("xtype", "textfield");
+						$fieldJS->add("editor", $editor);
+				}
+			}
+		}
+		
+		if (strlen($field->data["renderer"]) > 0) {
+			$fieldJS->addRaw("renderer", $field->data["renderer"]);
+		}
+	}
+
+	?>
+	var <?php echo $view->getName() . "View" ?> = new Ext.grid.EditorGridPanel(
+		<?php echo $config->printOut(); ?>
+	);
+	<?php
+}
+
+/**
+ * @param View $view
+ */
+function createForm($view) {
+	$config = new JavaScriptObject();
+	$config->add("border", TRUE);
+	$config->add("title", $view->getName());
+	$items = new JavaScriptArray();
+	$config->add("items", $items);
+	// $items->add(new JavaScriptObject("fieldLabel", "Test"));
+	foreach ($view->getFields() as $field) {
+		$fieldJS = new JavaScriptObject();
+		$items->add($fieldJS);
+		$fieldJS->add("xtype", "textfield");
+		$fieldJS->add("fieldLabel", $field->data["label"]);
+		$fieldJS->add("name", $field->data["name"]);
+	}
+	?>
+	var <?php echo $view->getName() . "View" ?> = new Ext.FormPanel(
+		<?php echo $config->printOut() ?>
+	);
+	<?php
+}
+
+/**
+ * @param View $view
+ */
 function printView($view) {
-	if ($view->data["viewType"] == "HorizontalContainer") {
-		// echo ", {xtype: 'container',}";
-	} else if ($view->data["viewType"] == "BasicTable") {
-		?>
-		, {
-			xtype : 'editorgrid',
-			flex : 1,
-			defaults : {
-				xtype : 'gridcolumn',
-				width: 120,
-				sortable : true
-			},
-			stripeRows : true,
-			sm : new Ext.grid.RowSelectionModel( {
-				listeners : {
-					rowselect : {
-						fn : function(sm, index, record) {
-							page.currentStore = <?php echo $view->getModel()->getName() ?>Store;
-							page.currentSelectionModel = sm;
-							<?php
-							foreach ($view->getModel()->childModels as $childModel) {
-								?>
-								<?php echo $childModel->getName() ?>Store.filter('DefineTableColumnTableID', record.data.DefineTableTableID);
-								<?php
-							}
-							?>
-						}
-					}
-				}
-			}),
-			viewConfig : {
-			},
-			columns : [ <?php
-				$started = false;
-				foreach ($view->getFields() as $field) {
-					echo $started ? ", " : "";
-					$started = true;
-					
-					$json["header"] = $field->data["label"];
-					$json["dataIndex"] = $field->data["name"];
-					$json["editable"] = $field->data["editable"] == 1 ? "true" : "false";
-					switch ($field->data["displayType"]) {
-						case "Checkbox":
-							$xtype = "checkbox";
-							break;
-						default:
-							$xtype = "textfield";
-					}
-					$json["editor"]["xtype"] = $xtype;
-					
-					HtmlUtils::jsonEncode($json);
-				}
-			?> ],
-			refresh : function() {
-				this.store.reload();
-			},
-			store : <?php echo $view->getModel()->getName() ?>Store
-		}
-		<?php
+	if ($view->data["viewType"] == "Form") {
+		echo ", " . $view->getName() . "View";
+	} else if ($view->data["viewType"] == "Grid") {
+		echo ", " . $view->getName() . "View";
 	} else {
-		?>
-		, {
-			xtype: 'form',
-			border: true,
-			items: [
-				{
-					fieldLabel: 'Test',
-				},
-				<?php
-				$started = false;
-				foreach ($view->getFields() as $field) {
-					echo $started ? ", " : "";
-					echo "{
-						xtype: 'textfield',
-						fieldLabel: '" . $field->data["label"] ."',
-						name: '" . $field->data["name"] ."'
-					}";
-					$started = true;
-				}
-				?>
-			]
-		}
-		<?php
+		
 	}
 	foreach ($view->childViews as $childView) {
 		printView($childView);
@@ -159,13 +314,34 @@ function printView($view) {
 }
 ?>
 (function() {
-	var saveQueue = new Object();
+	var saveQueue;
 	var writer = new Ext.data.JsonWriter({});
+
+	Ext.util.Format.comboRenderer = function(combo){
+	    return function(value){
+	        var record = combo.findRecord(combo.valueField, value);
+	        return record ? record.get(combo.displayField) : combo.valueNotFoundText;
+	    }
+	}
+	
+	var combo = new Ext.form.ComboBox({
+	    lazyRender: true,
+	    mode: 'local',
+	    store: new Ext.data.ArrayStore({
+	        id: 0,
+	        fields: [
+	            'value'
+	        ],
+	        data: [<?php ColumnTypes::printValues() ?>]
+	    }),
+	    valueField: 'value',
+	    displayField: 'value'
+	});
 	
 	<?php
-	printStore($view->getModel(), true);
+	createStore($view->getModel());
+	createView($view);
 	?>
-	<?php echo $view->getModel()->getName() ?>Store.load();
 	
 	var page = new Ext.Container({
 		currentStore : null,
@@ -190,70 +366,38 @@ function printView($view) {
     		},{
     			iconCls : 'icon-disk',
     			itemId : 'save',
-    			disabled : true,
+    			//disabled : true,
     			handler: function(button, event) {
-					var savejson = new Object();
-					var savejsonModel = new Object();
-					saveQueue = new Object();
-					<?php echo $view->getModel()->getName() ?>Store.save();
-					for ( var action in saveQueue) {
-						if (Ext.isDefined(action)) {
-							savejsonModel[action] = [];
-							for ( var i = 0; i < saveQueue[action].length; i++) {
-								savejsonModel[action].push(saveQueue[action][i].data);
-							}
-						}
-					}
-					savejson['<?php echo $view->getModel()->getName() ?>'] = savejsonModel;
-					
-					Ext.Ajax.request( {
-						url : 'data.php',
-						method: 'POST',
-						success : function(response, opts) {
-							var obj = Ext.decode(response.responseText);
-		
-							if (obj.success === false) {
-								alert("Failed to save data");
-								return;
-							}
-							for ( var action in saveQueue) {
-								if (Ext.isDefined(action)) {
-									this['on' + Ext.util.Format.capitalize(action) + 'Records'](obj.success, saveQueue[action],
-											obj.<?php echo $view->getModel()->getName() ?>[action]);
-								}
-							}
-						},
-						failure : function(response, opts) {
-							alert("Failed to save data with status code " + response.status);
-						},
-						params : {
-							action : 'save',
-							id : '<?php echo $view->getModel()->getId() ?>'
-						},
-						scope : <?php echo $view->getModel()->getName() ?>Store,
-						jsonData : Ext.util.JSON.encode(savejson)
-					});
+    				save<?php echo $view->getModel()->getName() ?>Store();
     			},
     			text: 'Save'
     		},{
     			iconCls : 'icon-minus',
-    			handler: function(b, e) {
-    				if (page.currentStore == null)
-						return;
-					var rows = page.currentSelectionModel.getSelections();
-					page.currentStore.remove(rows);
-    			},
+    			menu : {
+	                items: [{
+		    			handler: deleteDefineTableStore,
+	                    text: 'Delete Table'
+	                }, {
+		    			handler: deleteDefineTableColumnStore,
+	                    text: 'Delete Column'
+	                }]
+	            },
+    			tooltip: {text:'This is a an example QuickTip for a toolbar item', title:'Tip Title'},
     			text: 'Delete'
     		},{
     			iconCls : 'icon-plus',
-    			handler: function(b, e) {
-					if (this.currentStore === null)
-						return;
-					<?php echo $view->getModel()->getName() ?>Store.reload();
-    			},
+    			menu : {
+	                items: [{
+		    			handler: insertDefineTableStore,
+	                    text: 'Add Table'
+	                }, {
+		    			handler: insertDefineTableColumnStore,
+	                    text: 'Add Column'
+	                }]
+	            },
     			text: 'Add'
     		},
-    		'->', // same as {xtype: 'tbfill'}, // Ext.Toolbar.Fill
+    		'->',
     		{
     			iconCls : 'icon-arrow-left',
     			text: 'Previous'
@@ -266,7 +410,17 @@ function printView($view) {
 		<?php printView($view) ?>
 		]
 	});
-
+	
 	page.currentStore = <?php echo $view->getModel()->getName() ?>Store;
+	
+	map.addBinding({
+		shift : true,
+		ctrl : true,
+		key : "i",
+		fn : function(keycode, e) {
+			alert("Insert");
+		}
+	});
+	
 	return page;
 })();
