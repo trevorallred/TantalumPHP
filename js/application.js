e1f47c01cc94cbc7187925609901c5d9102bbb3d
Trevor Allred <trevorallred@gmail.com>
2011-01-25 11:19:58 -0800

    moved keymap to page_js

--- js/application.js
+++ js/application.js
@@ -28,53 +28,12 @@ BodyPanel = Ext.extend(Ext.Viewport, {
 	}
 });
 
+var map;
+
 Ext.onReady(function() {
 	Ext.QuickTips.init();
 
 	var bodyPanel = new BodyPanel();
 
-	var map = new Ext.KeyMap(document, [ {
-		shift : true,
-		ctrl : true,
-		key : "i",
-		fn : function(keycode, e) {
-			insertRecord();
-		}
-	}, {
-		shift : true,
-		ctrl : true,
-		key : "d",
-		fn : function(keycode, e) {
-			deleteRecord();
-		}
-	}, {
-		shift : true,
-		ctrl : true,
-		key : "s",
-		fn : function(keycode, e) {
-			savePage();
-		}
-	}, {
-		shift : true,
-		ctrl : true,
-		key : "r",
-		fn : function(keycode, e) {
-			refresh();
-		}
-	}, {
-		ctrl : true,
-		key : "\t",
-		fn : function(keycode, e) {
-			var contentPanel = Ext.ComponentMgr.get('contentPanel');
-			if (contentPanel.items.length < 2)
-				return;
-			var tab = contentPanel.getActiveTab();
-			tab = tab.nextSibling();
-			if (tab === null)
-				contentPanel.setActiveTab(0);
-			else
-				contentPanel.setActiveTab(tab);
-		}
-	} ]);
-	
+	map = new Ext.KeyMap(document);
 });
