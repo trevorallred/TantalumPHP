0dce639c98ffa43cff683a00f53522f8bed01c08
Trevor Allred <trevorallred@gmail.com>
2011-02-14 09:10:04 -0800

    upgrade ext 3.3.1

--- index.php
+++ index.php
@@ -10,12 +10,12 @@ require_once 'util/startup.php';
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>Tantalum</title>
-<link rel="stylesheet" type="text/css" href="ext/resources/css/ext-all.css">
+<link rel="stylesheet" type="text/css" href="ext-3.3.1/resources/css/ext-all.css">
 <link rel="stylesheet" type="text/css" href="themes/default/icons.css" />
 <link rel="stylesheet" type="text/css" href="themes/default/application.css">
 <link rel="shortcut icon" type="text/css" href="themes/default/application.ico" />
-<script type="text/javascript" src="ext/adapter/ext/ext-base.js"></script>
-<script type="text/javascript" src="ext/ext-all-debug.js"></script>
+<script type="text/javascript" src="ext-3.3.1/adapter/ext/ext-base.js"></script>
+<script type="text/javascript" src="ext-3.3.1/ext-all-debug.js"></script>
 <script type="text/javascript" src="js/Ext.ux.data.BindMgr.js"></script>
 
 <script type="text/javascript" src="menu.php"></script>
